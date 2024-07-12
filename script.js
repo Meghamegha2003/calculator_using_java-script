41bb88d9d73c5e1d7bc5e91b552dd34d17650829
Megha Gopalakrishnan <158165227+Meghamegha2003@users.noreply.github.com>
2024-07-12 17:39:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function buttonclick(val){
+    document.getElementById("screen").value+=val
+}function clearAll(){
+    document.getElementById("screen").value=""
+}function equalclick(){
+    var text=document.getElementById('screen').value
+    var result=eval(text)
+    document.getElementById('screen').value=result
+}
